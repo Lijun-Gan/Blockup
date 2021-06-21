f16bd44c05e3e8ee5b9218ce83f5e73c10759d3b
Lijun <gan.lijun.glj@gmail.com>
2021-06-21 00:08:07 -0400

    allow two accounts log in  on the signup page

--- frontend/src/components/session/signup_form_container.js
+++ frontend/src/components/session/signup_form_container.js
@@ -1,6 +1,8 @@
 import { connect } from "react-redux";
 import { signup, login  } from "../../actions/session_actions";
 import SignupForm from "./signup_form";
+import * as sessionActions from "../../actions/session_actions";
+
 
 const mapStateToProps = (state) => {
   return {
@@ -13,6 +15,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     signup: (user) => dispatch(signup(user)),
     login: (user) => dispatch(login(user)),
+    logout: () => dispatch(sessionActions.logout()),
   };
 };
 
